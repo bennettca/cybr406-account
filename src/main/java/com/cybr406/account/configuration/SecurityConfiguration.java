f4e44a503d07c479c3b9b64090443001e700130e
Cass Bennett <bennettca@lopers.unk.edu>
2020-04-15 00:34:35 -0500

    HW2 Completion

--- src/main/java/com/cybr406/account/configuration/SecurityConfiguration.java
+++ src/main/java/com/cybr406/account/configuration/SecurityConfiguration.java
@@ -1,33 +1,80 @@
 package com.cybr406.account.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 	{
+	@Value("${spring.h2.console.enabled}")
+	boolean h2ConsoleEnabled;
 	
-		@Autowired
-		DataSource dataSource;
+	@Autowired
+	DataSource dataSource;
 	
+	@Autowired
+	H2SecurityConfigurer h2SecurityConfigurer;
+	
+	@Bean
+	public UserDetailsManager userDetailsManager() {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder()
+		{
+			return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		}
+	
+	@Bean
+	User.UserBuilder userBuilder() {
+		PasswordEncoder passwordEncoder = passwordEncoder();
 		
-		@Autowired
+		User.UserBuilder users = User.builder();
+		
+		users.passwordEncoder(passwordEncoder::encode);
+		
+		return users;
+	}
+	
+	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
-			{
-				auth.jdbcAuthentication().dataSource(dataSource);
-			}
+		{
+			auth.jdbcAuthentication().dataSource(dataSource);
+		}
+	
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception
+	protected void configure(HttpSecurity HTTP) throws Exception
 		{
+			h2SecurityConfigurer.configure(HTTP);
+			
+			if (h2ConsoleEnabled) {
+				HTTP.authorizeRequests()
+						.antMatchers("/h2-console/**", "/h2-console").permitAll();
+				
+				HTTP.headers().frameOptions().sameOrigin();
+			}
+			
+			
 			/**
 			 * Problem 07: disable sessions
 			 *
@@ -38,11 +85,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 			 *     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			 */
 			
-				
 			
-			http.authorizeRequests()
-					.mvcMatchers(HttpMethod.GET, "/", "/**").permitAll()
-					.mvcMatchers(HttpMethod.POST, "/signup").permitAll()
+			HTTP.authorizeRequests()
+					.mvcMatchers(HttpMethod.GET, "/check-user").hasAnyRole("ADMIN", "SERVICE")
+					.mvcMatchers(HttpMethod.GET , "/" , "/**").permitAll()
+					.mvcMatchers(HttpMethod.POST , "/signup").permitAll()
 					.anyRequest().authenticated()
 					.and()
 					.csrf().disable()
@@ -51,5 +98,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 					.sessionManagement()
 					.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		}
-	
 	}
